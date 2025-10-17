ef7c8fc67954992f5ce16d1e652bb52755ff1201
tranduyhunghihi <hungduytran159@gmail.com>
2025-10-17 22:59:01 +0700

    add feature work

--- featurework.js
+++ featurework.js
@@ -7,6 +7,10 @@ const after = $('.hover');
 const contact = $('.default');
 const text1 = $('.text1');
 const text2 = $('.text2');
+const slider = $('.slider');
+const track = $('.track');
+
+
 
 setTimeout(() => {
     text1.style.animation = 'SlideUp ease 0.3s forwards';
@@ -16,14 +20,41 @@ setTimeout(() => {
 
 document.onscroll = function(){
     const scroll = (document.documentElement.scrollTop).toFixed(0) || window.scrollY;
-    // console.log(scroll);
-    if(scroll > 1100){
+    console.log(scroll);
+    if(scroll > 360){
         body.style.backgroundColor = '#f9f9dd';
+        slider.classList.add('change-color')
     }else{
         body.style.backgroundColor = '';
+        slider.classList.remove('change-color');
 
     }
 }
+//tao cuon hinh anh vo han
+let baseSpeed = 0.5;
+let currentSpeed = baseSpeed;
+let position = 0;
+track.innerHTML += track.innerHTML;
+
+
+function loop() {
+    position += currentSpeed;
+    if(position >= track.scrollWidth){
+        position = 0;
+    }
+    track.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(loop);
+} 
+
+loop();
+
+slider.addEventListener('mouseenter', () => {
+    currentSpeed = baseSpeed*0.25;
+});
+slider.addEventListener('mouseleave', () => {
+    currentSpeed = baseSpeed;
+});
+
 
 before.onmouseenter = function(){
     console.log('hihi');
