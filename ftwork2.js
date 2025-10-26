9171189adb18b76de06576efc0c073c5fa1d7281
tranduyhunghihi <hungduytran159@gmail.com>
2025-10-26 10:56:25 +0700

    them hinh anh de len nhau

--- ftwork2.js
+++ ftwork2.js
@@ -3,7 +3,9 @@ const $$ = document.querySelectorAll.bind(document);
 
 const header = $('.header');
 const logo = $('.logo');
-
+const imgs = $$('.book-img');
+let index = 1;
+const time = 300;
 
 document.onscroll = function() {
     const scroll = ((document.documentElement.scrollTop).toFixed(0)) || window.scrollY; 
@@ -17,7 +19,6 @@ document.onscroll = function() {
 
 }
 
-
 //khi nhan vao header thi cuon ve dau trang
 
 header.onclick = function(){
@@ -26,3 +27,38 @@ header.onclick = function(){
         behavior: "smooth"
     })
 }
+//hinh anh de len nhau vo han
+
+function showImg(){
+    imgs.forEach((img,i )=>{
+        if(i === 0){
+            img.classList.remove('display');
+        }else{
+            img.classList.add('display');
+
+        }
+    })
+
+    index = 1;
+
+    const showInterval = setInterval(() => {
+        imgs[index].classList.remove('display');
+        index++;
+
+        if(index >= imgs.length){
+            clearInterval(showInterval);
+
+            setTimeout(() => {
+                imgs.forEach((img, i) => {
+                    if(i !== 0) {
+                        img.classList.add('display');
+                    }
+                })
+                showImg();
+
+            },time)
+        }
+
+    },time)
+}
+showImg();
